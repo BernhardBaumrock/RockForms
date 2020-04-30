424b23ae40f6990318cd40e1330e7f68a4e1f111
BernhardBaumrock <office@baumrock.com>
2020-04-30 17:26:44 +0200

    refactor

--- assets/renderers/uikit.php
+++ assets/renderers/uikit.php
@@ -1,26 +1,18 @@
 <?php namespace ProcessWire;
 /** @var \Nette\Forms\Rendering\DefaultFormRenderer $renderer */
 foreach($form->getComponents() as $component) {
-  switch(true) {
-    case $component instanceof \Nette\Forms\Controls\RadioList:
-      $component->getControlPrototype()->addClass('uk-radio');
-      $component->getSeparatorPrototype()->setName('span class="uk-margin-small-left"');
-      $component->getContainerPrototype()->addClass('uk-alert-danger');
-    break;
-    case $component instanceof \Nette\Forms\Controls\TextInput:
-      $component->getControlPrototype()
-        ->addClass('uk-input')
-        ->addPlaceholder($component->label->getText())
-        ;
-    break;
-    case $component instanceof \Nette\Forms\Controls\TextArea:
-      $component->getControlPrototype()
-        ->addClass('uk-textarea')
-        ->addPlaceholder($component->label->getText())
-        ;
-    break;
-    case $component instanceof \Nette\Forms\Controls\SubmitButton:
-      $component->getControlPrototype()->addClass('uk-button');
-    break;
+  if($component instanceof \Nette\Forms\Controls\RadioList) {
+    $component->getControlPrototype()->addClass('uk-radio');
+    $component->getSeparatorPrototype()->setName('span class="uk-margin-small-left"');
+    $component->getContainerPrototype()->addClass('uk-alert-danger');
+  }
+  elseif($component instanceof \Nette\Forms\Controls\TextInput) {
+    $component->getControlPrototype()->addClass('uk-input');
+  }
+  elseif($component instanceof \Nette\Forms\Controls\TextArea) {
+    $component->getControlPrototype()->addClass('uk-textarea');
+  }
+  elseif($component instanceof \Nette\Forms\Controls\SubmitButton) {
+    $component->getControlPrototype()->addClass('uk-button');
   }
 }
